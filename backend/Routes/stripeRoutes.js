28ce89f3f7893e80f1dfdb4268ac8b4518a1c657
Usama Bin Ghazan <usamaghazan@yahoo.com>
2023-07-08 06:57:23 +0500

    Resolving stripe issues

--- backend/Routes/stripeRoutes.js
+++ backend/Routes/stripeRoutes.js
@@ -1,52 +1,47 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import stripe from 'stripe';
 import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
-//Change later to env
+//Change later to env(check kro 2 env files ki wja sy masla to ni araha)
 const stripeAPI = new stripe(
   'sk_test_51NBUB0SH4AJANTU5jWfqmOGx7CITuyLxGWCtOwSxY9YO35iJLcaY0FOzMSgPhoBwpufWR6ztxCV1NrsnkM20QSGh00cswwpQa0'
 );
 
-router.get('/', (req, res, next) => {
-  console.log('Get response from researcher');
-  res.json({
-    message: 'It works',
-  });
-});
-
-router.post('/pay', (req, res) => {
-  console.log('showing req.body.token', req.body);
-  const { token, amount } = req.body;
-  const idempontencyKey = uuidv4();
-  return stripeAPI.customers
-    .create({
-      email: token.email,
-      source: token.source,
-    })
-    .then((customer) => {
-      return stripeAPI.paymentIntents.create(
-        {
-          amount: amount * 100,
-          currency: 'usd',
-          customer: customer.id,
-          receipt_email: token.email,
-        },
-        {
-          idempotencyKey: idempontencyKey,
-        }
-      );
-    })
-    .then((result) => {
-      console.log('In result block ', result);
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ error: 'An error occurred during payment processing.' });
-    });
-});
+router.post(
+  '/pay',
+  asyncHandler(async (req, res) => {
+    const { token, amount } = req.body;
+    const idempontencyKey = uuidv4();
+    return stripeAPI.customers
+      .create({
+        email: token.email,
+        source: token.source,
+      })
+      .then((customer) => {
+        return stripeAPI.paymentIntents.create(
+          {
+            amount: amount * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+          },
+          {
+            idempotencyKey: idempontencyKey,
+          }
+        );
+      })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ error: 'An error occurred during payment processing.' });
+      });
+  })
+);
 
 export default router;
--- frontend/src/Screens/PaymentScreen.js
+++ frontend/src/Screens/PaymentScreen.js
@@ -21,15 +21,17 @@ import {
   ModalCloseButton,
 } from '@chakra-ui/react';
 import { getDoctorDetails } from '../Features/DoctorFeature/doctorDetailSlice';
-const stripePromise = loadStripe(
-  'pk_test_51NBUB0SH4AJANTU5XiEHxXYpURC9qeSUkPWDuHFJ8gwlwIf3XOKcmBsDMOhh7NwTz3uhtrFZezpCLkzj8ByLBbCf00Mmu9i13b'
-);
 
 const PaymentScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const params = useParams();
   const doctorId = params.id;
+  const { doctor, loading, error } = useSelector(store => store.doctorDetails);
+
+  useEffect(() => {
+    dispatch(getDoctorDetails(doctorId));
+  }, [doctorId, dispatch]);
 
   const handleToken = async (totalAmount, token) => {
     console.log(token);
@@ -54,141 +56,60 @@ const PaymentScreen = () => {
       // navigate('/success');
     } catch (error) {
       console.log(error.response.data);
-      // Handle error, display error message, etc.
     }
   };
 
   const tokenHandler = token => {
-    handleToken(100, token);
+    handleToken(doctor.charges, token);
   };
 
   return (
-    <Box>
-      <Stripe
-        stripeKey="pk_test_51NBUB0SH4AJANTU5XiEHxXYpURC9qeSUkPWDuHFJ8gwlwIf3XOKcmBsDMOhh7NwTz3uhtrFZezpCLkzj8ByLBbCf00Mmu9i13b"
-        token={tokenHandler}
-      />
+    <Box
+      maxWidth="600px"
+      mx="auto"
+      p={4}
+      borderRadius="lg"
+      boxShadow="lg"
+      bg="white"
+    >
+      <Heading as="h1" size="xl" mb={4}>
+        Payment
+      </Heading>
+      {loading ? (
+        <Box textAlign="center">
+          <Spinner size="lg" color="teal.500" />
+          <Text mt={2}>Loading doctor details...</Text>
+        </Box>
+      ) : error ? (
+        <Text color="red.500">Error: {error}</Text>
+      ) : (
+        <>
+          <Text fontSize="xl" fontWeight="bold" mb={4}>
+            Doctor Details
+          </Text>
+          <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
+            <Text>
+              <strong>Name:</strong> {doctor?.name}
+            </Text>
+          </Box>
+          <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
+            <Text>
+              <strong>Specialization:</strong> {doctor?.specialization}
+            </Text>
+          </Box>
+          <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
+            <Text>
+              <strong>Charges:</strong> ${doctor?.charges}
+            </Text>
+          </Box>
+          <Stripe
+            stripeKey="pk_test_51NBUB0SH4AJANTU5XiEHxXYpURC9qeSUkPWDuHFJ8gwlwIf3XOKcmBsDMOhh7NwTz3uhtrFZezpCLkzj8ByLBbCf00Mmu9i13b"
+            token={tokenHandler}
+          />
+        </>
+      )}
     </Box>
   );
-  // const [temp, setTemp] = useState({
-  //   name: 'Usama',
-  //   charges: 100,
-  // });
-  // const [isModalOpen, setIsModalOpen] = useState(false); // State for the modal
-
-  // const { doctor, loading, error } = useSelector(store => store.doctorDetails);
-  // useEffect(() => {
-  //   dispatch(getDoctorDetails(doctorId));
-  // }, [dispatch, doctorId]);
-
-  // // ...
-
-  // const makePayment = async token => {
-  //   console.log(temp);
-  //   const body = {
-  //     token,
-  //     doctor: temp, // Use "doctor" instead of "temp"
-  //   };
-
-  //   try {
-  //     const response = await axios.post('/payment', body);
-  //     console.log('RESPONSE ', response);
-  //     const data = response.data; // Get the response data
-  //     const { status } = response;
-  //     console.log(status, data);
-
-  //     // Open the modal
-  //     setIsModalOpen(true);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const closeModal = () => {
-  //   // Close the modal and navigate to another page
-  //   setIsModalOpen(false);
-  //   navigate('/some-other-page');
-  // };
-
-  // return (
-  //   <Box
-  //     maxWidth="600px"
-  //     mx="auto"
-  //     p={4}
-  //     borderRadius="lg"
-  //     boxShadow="lg"
-  //     bg="white"
-  //   >
-  //     <Heading as="h1" size="xl" mb={4}>
-  //       Payment
-  //     </Heading>
-  //     {loading ? (
-  //       <Box textAlign="center">
-  //         <Spinner size="lg" color="teal.500" />
-  //         <Text mt={2}>Loading doctor details...</Text>
-  //       </Box>
-  //     ) : error ? (
-  //       <Text color="red.500">Error: {error}</Text>
-  //     ) : (
-  //       <>
-  //         <Text fontSize="xl" fontWeight="bold" mb={4}>
-  //           Doctor Details
-  //         </Text>
-  //         <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
-  //           <Text>
-  //             <strong>Name:</strong> {doctor?.name}
-  //           </Text>
-  //         </Box>
-  //         <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
-  //           <Text>
-  //             <strong>Specialization:</strong> {doctor?.specialization}
-  //           </Text>
-  //         </Box>
-  //         <Box border="1px solid gray" borderRadius="md" p={2} mb={4}>
-  //           <Text>
-  //             <strong>Charges:</strong> ${doctor?.charges}
-  //           </Text>
-  //         </Box>
-  //         <StripeCheckout
-  //           stripeKey="pk_test_51NBUB0SH4AJANTU5XiEHxXYpURC9qeSUkPWDuHFJ8gwlwIf3XOKcmBsDMOhh7NwTz3uhtrFZezpCLkzj8ByLBbCf00Mmu9i13b"
-  //           token={makePayment}
-  //           name="Pay Now"
-  //           amount={doctor?.charges * 100}
-  //         >
-  //           <Button
-  //             mt={4}
-  //             colorScheme="teal"
-  //             size="lg"
-  //             w="100%"
-  //             _hover={{ bg: 'teal.600' }}
-  //           >
-  //             Pay Now
-  //           </Button>
-  //         </StripeCheckout>
-
-  //         {/* Modal */}
-  //         <Modal isOpen={isModalOpen} onClose={closeModal}>
-  //           <ModalOverlay />
-  //           <ModalContent>
-  //             <ModalHeader>Payment Successful</ModalHeader>
-  //             <ModalCloseButton />
-  //             <ModalBody>
-  //               <Text fontSize="xl" fontWeight="bold" mb={4}>
-  //                 Fee Payed
-  //               </Text>
-  //               <Text>Your appointment has been booked.</Text>
-  //             </ModalBody>
-  //             <ModalFooter>
-  //               <Button colorScheme="teal" onClick={closeModal}>
-  //                 Close
-  //               </Button>
-  //             </ModalFooter>
-  //           </ModalContent>
-  //         </Modal>
-  //       </>
-  //     )}
-  //   </Box>
-  // );
 };
 
 export default PaymentScreen;

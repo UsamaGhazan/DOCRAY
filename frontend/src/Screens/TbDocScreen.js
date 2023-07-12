7b6684c1a0f9ab1d676247e4225d776cb33006bb
Usama Bin Ghazan <usamaghazan@yahoo.com>
2023-07-12 10:59:18 +0500

    Code reuseabilitly improved

--- frontend/src/Screens/TbDocScreen.js
+++ frontend/src/Screens/TbDocScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import DoctorCard from '../Components/DoctorCard.js';
 import {
   Card,
   CardHeader,
@@ -41,90 +42,7 @@ const TbDocScreen = () => {
               return doctor.category === 'Tb';
             })
             .map(doctor => {
-              return (
-                <Card
-                  as={Link}
-                  to={`/doctors/${doctor._id}`}
-                  key={doctor.id}
-                  w="1123px"
-                  h="290px"
-                  border="2px solid #E8E8E8"
-                  backgroundColor="#fff"
-                  mt="20px"
-                >
-                  <HStack spacing="480px">
-                    <Box ml="32px">
-                      <Avatar
-                        name={doctor.name}
-                        src={doctor.image}
-                        size="xl"
-                        mt="10px"
-                      />
-                    </Box>
-                    <VStack
-                      align="flex-start"
-                      className="cardText"
-                      spacing="30px"
-                    >
-                      <Box>
-                        <Text fontSize="18px" fontWeight={400} mt="21px">
-                          {doctor.name}
-                        </Text>
-                        <Text fontSize="14px" lineHeight="20px">
-                          {doctor.specialization}
-                        </Text>
-                        <Text fontSize="14px" lineHeight="20px">
-                          {doctor.degree}
-                        </Text>
-                      </Box>
-                      <HStack mt="10px">
-                        <VStack>
-                          <Text fontSize="14px" fontWeight="600">
-                            {doctor.experience} years
-                          </Text>
-                          <Text fontSize="12px" color="#8c9196">
-                            Experience
-                          </Text>
-                        </VStack>
-                        <VStack>
-                          <Text fontSize="14px" fontWeight="600" px="50px">
-                            {Math.round(
-                              (doctor.satisfied / doctor.patientsChecked) * 100
-                            )}
-                            % ({doctor.satisfied})
-                          </Text>
-                          <Text fontSize="12px" color="#8c9196">
-                            Satisfied Patients
-                          </Text>
-                        </VStack>
-                      </HStack>
-                    </VStack>
-                    <VStack>
-                      <Button
-                        as={Link}
-                        to={`/doctors/${doctor._id}`}
-                        variant="outline"
-                        size="lg"
-                        color="brand.60"
-                        fontSize="14px"
-                        className="bookbtn"
-                        _hover={{ bg: 'none' }}
-                      >
-                        View Details{' '}
-                      </Button>
-                      <Button
-                        as={Link}
-                        to={`/payment/${doctor._id}`}
-                        color="brand.50"
-                        fontSize="14px"
-                        className="goldbtn bookbtn"
-                      >
-                        Book Appointment
-                      </Button>
-                    </VStack>
-                  </HStack>
-                </Card>
-              );
+              return <DoctorCard doctor={doctor} />;
             })
         )}
       </div>

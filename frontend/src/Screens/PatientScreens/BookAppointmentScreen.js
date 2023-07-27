442c302f34629ae63e970e3a00dfa7bb01c5a5e4
Usama Bin Ghazan <usamaghazan@yahoo.com>
2023-07-27 07:28:20 +0500

    Creating BookAppointmentScreen

--- frontend/src/Screens/PatientScreens/BookAppointmentScreen.js
+++ frontend/src/Screens/PatientScreens/BookAppointmentScreen.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDoctorDetails } from '../../Features/DoctorFeature/doctorDetailSlice';
 import {
   Box,
   VStack,
@@ -8,9 +11,69 @@ import {
   Select,
   Textarea,
   Button,
+  Text,
+  Avatar,
+  Center,
+  Spinner,
 } from '@chakra-ui/react';
 import PatientForm from '../../Components/PatientForm';
+
 const BookAppointmentScreen = () => {
-  return <PatientForm />;
+  const params = useParams();
+  const dispatch = useDispatch();
+  const doctorId = params.id;
+  const { doctor, loading, error } = useSelector(store => store.doctorDetails);
+
+  useEffect(() => {
+    dispatch(getDoctorDetails(doctorId));
+  }, [dispatch, doctorId]);
+
+  return (
+    <section className="bookAppointmentScreen">
+      {error && <Box>{error}</Box>}
+      <div>
+        {loading ? (
+          <div className="spinner-container">
+            <Spinner size="xl" />
+          </div>
+        ) : (
+          <Box
+            maxW="744px"
+            h="124px"
+            p="10px"
+            bg="white"
+            borderRadius="md"
+            boxShadow="md"
+            display="flex"
+            alignItems="center"
+            marginLeft="388px"
+            mt="30px"
+          >
+            {/* Avatar (Left Side) */}
+            <Avatar size="lg" name={doctor.name} src={doctor.image} mr="10px" />
+
+            {/* Doctor Information (Right Side) */}
+            <Box>
+              {/* Doctor Name */}
+              <Text fontSize="lg" fontWeight="bold">
+                {doctor.name}
+              </Text>
+
+              {/* Clinic Name */}
+              <Text fontSize="md" color="gray.500" mb="2px">
+                docRay Care Video Consultation (Online){' '}
+              </Text>
+
+              {/* Fee Amount */}
+              <Text fontSize="md" color="green.500">
+                Fee: ${doctor.charges}
+              </Text>
+            </Box>
+          </Box>
+        )}
+      </div>
+    </section>
+  );
 };
+
 export default BookAppointmentScreen;

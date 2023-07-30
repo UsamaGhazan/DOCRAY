5694b5bd971916704a52fab061a4aa484477cbd0
Usama Bin Ghazan <usamaghazan@yahoo.com>
2023-07-30 19:54:26 +0500

    Backend started for bookappointment

--- backend/Controllers/patientController.js
+++ backend/Controllers/patientController.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Patient from '../Models/patientModel.js';
 import Appointment from '../Models/appointmentModel.js';
 import generateToken from '../utils/generateToken.js';
+import moment from 'moment-timezone';
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -70,7 +71,21 @@ const getPatientProfile = asyncHandler(async (req, res) => {
 });
 
 const bookAppointment = asyncHandler(async (req, res) => {
-  console.log(req.user);
-  console.log(req.body);
+  const { email } = req.user;
+  const { startTime, date, doctorID } = req.body;
+  // Combining the date and time strings into a single string
+  const dateTimeString = `${date} ${startTime}`;
+  console.log(dateTimeString);
+  // Parsing the dateTimeString into Javascript Date object
+  const dateObj = new Date(dateTimeString);
+  console.log(dateObj);
+  // Checking if the dateObj is valid object
+  if (isNaN(dateObj)) {
+    res.status(400).json({ error: 'Invalid date or time format' });
+  }
+  const iso8601DateTime = dateObj.toISOString();
+  console.log(iso8601DateTime);
+  res.json({ iso8601DateTime });
 });
+
 export { authUser, getPatientProfile, registerUser, bookAppointment };

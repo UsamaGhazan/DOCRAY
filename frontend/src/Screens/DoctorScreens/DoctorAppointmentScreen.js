2ed0806ad0aa5516a6bde385025310c4bb9cc57e
Usama Bin Ghazan <usamaghazan@yahoo.com>
2023-08-07 20:54:57 +0500

    Patient details card UI completed in doctorAppointmentScreen

--- frontend/src/Screens/DoctorScreens/DoctorAppointmentScreen.js
+++ frontend/src/Screens/DoctorScreens/DoctorAppointmentScreen.js
@@ -6,6 +6,7 @@ import {
 import {
   Avatar,
   Box,
+  Button,
   Flex,
   Grid,
   HStack,
@@ -17,44 +18,51 @@ import React from 'react';
 
 const DoctorAppointmentScreen = () => {
   return (
-    <>
-      <section className="docApptScreen">
-        <Box ml={{ base: 0, md: 60 }} p="4">
-          <Box w={1200} h="110px" border="2px solid red">
-            <Grid templateColumns="auto 1fr" gap="4" alignItems="center" p="4">
-              {/* Avatar */}
-              <Avatar
-                name="John Doe"
-                src="https://via.placeholder.com/50"
-                size="lg"
-              />
+    <Box ml={{ base: 0, md: 60 }} p="4">
+      <Box w={1200} h="110px" border="2px solid red" borderRadius="8px">
+        <Grid templateColumns="auto 1fr auto" gap="4" alignItems="center" p="4">
+          {/* Avatar */}
+          <Avatar
+            name="John Doe"
+            src="https://via.placeholder.com/50"
+            size="lg"
+          />
 
-              {/* Patient name and Date-time section */}
-              <Flex align="center">
-                <Flex direction="column">
-                  {/* Patient name */}
-                  <Text fontSize="lg" fontWeight="bold">
-                    John Doe
-                  </Text>
-                  {/* Date and Time */}
-                  <HStack mt="2" spacing={10}>
-                    <HStack>
-                      <Icon as={AiTwotoneCalendar} boxSize="5" />
-                      <span fontSize="md"> August 8, 2023 - 2:30 PM</span>
-                    </HStack>
-                    <HStack>
-                      <Icon as={AiOutlineVideoCameraAdd} boxSize="5" />
-                      <span fontSize="md"> Online Video Consultation</span>
-                    </HStack>
-                  </HStack>
-                </Flex>
-              </Flex>
-            </Grid>
-          </Box>
-        </Box>
-      </section>
-    </>
+          {/* Patient name and Date-time section */}
+          <Flex align="center">
+            <Flex direction="column">
+              {/* Patient name */}
+              <Text fontSize="lg" fontWeight="bold">
+                John Doe
+              </Text>
+              {/* Date and Time */}
+              <HStack mt="2" spacing={10}>
+                <HStack>
+                  <Icon as={AiOutlineCalendar} boxSize="5" />
+                  <span fontSize="md"> August 8, 2023 - 2:30 PM</span>
+                </HStack>
+                <HStack>
+                  <Icon as={AiOutlineVideoCameraAdd} boxSize="5" />
+                  <span fontSize="md"> Online Video Consultation</span>
+                </HStack>
+              </HStack>
+            </Flex>
+          </Flex>
+
+          {/* Cancel Appointment Button */}
+          <Button
+            bg="#000066"
+            color="white"
+            size="md"
+            _hover={{ bg: '#000066' }}
+            mr={10}
+            mt={3}
+          >
+            Cancel Appointment
+          </Button>
+        </Grid>
+      </Box>
+    </Box>
   );
 };
-
 export default DoctorAppointmentScreen;
